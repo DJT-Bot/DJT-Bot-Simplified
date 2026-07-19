b02757802bd598d64c804ef8d5523877c05d2fb9
Ken <52684990+LHBlitz@users.noreply.github.com>
2026-07-19 16:29:51 -0400

    initial deploy file

---
+++ deploy.js
@@ -0,0 +1,24 @@
+const { REST, Routes, SlashCommandBuilder } = require('discord.js');
+require('dotenv').config();
+
+const commands = [
+    new SlashCommandBuilder().setName('about').setDescription('Shows bot info'),
+    new SlashCommandBuilder().setName('uptime').setDescription('Shows how long DJT has been online'),
+].map(command => command.toJSON());
+
+const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
+
+(async () => {
+    try {
+        console.log('Started refreshing application (/) commands.');
+
+    await rest.put(
+        Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
+        { body: commands },
+);
+
+        console.log('Successfully reloaded application (/) commands.');
+    } catch (error) {
+        console.error(error);
+    }
+})();
